0ae8031d0aed6849026220e52df7777d3d39e103
André Cerqueira <78262628+AndreCerqueira@users.noreply.github.com>
2023-03-07 17:41:54 +0000

    mapa

--- FluffyFighters/Game1.cs
+++ FluffyFighters/Game1.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using SharpDX.Direct3D9;
+using System.IO;
 
 namespace FluffyFighters
 {
@@ -8,6 +10,13 @@ namespace FluffyFighters
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        private SpriteFont font;
+        private int nrLinhas = 0;
+        private int nrColunas = 0;
+        private char[,] level;
+        private Texture2D player, grass, rock;
+        private int tileSize = 64;
+
 
         public Game1()
         {
@@ -19,6 +28,12 @@ namespace FluffyFighters
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            LoadLevel("level1.txt");
+            
+            _graphics.PreferredBackBufferHeight = tileSize * level.GetLength(1); //definição da altura
+            _graphics.PreferredBackBufferWidth = tileSize * level.GetLength(0); //definição da largura
+            _graphics.ApplyChanges(); //aplica a atualização da janela
+
 
             base.Initialize();
         }
@@ -26,9 +41,33 @@ namespace FluffyFighters
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+            font = Content.Load<SpriteFont>("File");
+            player = Content.Load<Texture2D>("Character");
+            grass = Content.Load<Texture2D>("Grass");
+            rock = Content.Load<Texture2D>("Rock");
 
             // TODO: use this.Content to load your game content here
         }
+
+
+
+        void LoadLevel(string levelFile)
+        {
+            string[] linhas = File.ReadAllLines($"Content/{levelFile}"); // "Content/" + level
+            nrLinhas = linhas.Length;
+            nrColunas = linhas[0].Length;
+
+            level = new char[nrColunas, nrLinhas];
+            for (int x = 0; x < nrColunas; x++)
+            {
+                for (int y = 0; y < nrLinhas; y++)
+                {
+                    level[x, y] = linhas[y][x];
+                }
+            }
+
+        }
+
 
         protected override void Update(GameTime gameTime)
         {
@@ -43,6 +82,43 @@ namespace FluffyFighters
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin();
+            _spriteBatch.DrawString(font, "O texto que quiser", new Vector2(100, 100), Color.Black);
+            _spriteBatch.DrawString(font, $"Numero de Linhas = {nrLinhas} -- Numero de Colunas = {nrColunas}", new Vector2(0, 0), Color.Black);
+
+
+            Rectangle position = new Rectangle(0, 0, tileSize, tileSize); //calculo do retangulo a depender do tileSize
+            for (int x = 0; x < level.GetLength(0); x++) //pega a primeira dimensão
+            {
+                for (int y = 0; y < level.GetLength(1); y++) //pega a segunda dimensão
+                {
+                    position.X = x * tileSize; // define o position
+                    position.Y = y * tileSize; // define o position
+                    switch (level[x, y])
+                    {
+                        case 'Y':
+                            _spriteBatch.Draw(player, position, Color.White);
+                            break;
+                        case '#':
+                            _spriteBatch.Draw(rock, position, Color.White);
+                            break;
+                        case '.':
+                            _spriteBatch.Draw(grass, position, Color.White);
+                            break;
+                        case 'X':
+                            _spriteBatch.Draw(grass, position, Color.White);
+                            break;
+                    }
+                }
+            }
+
+
+
+
+            _spriteBatch.End();
+
+
 
             // TODO: Add your drawing code here
 
