d768e8e5bcbdc284067e42c03fdbd483323f2ef3
Alexandre Marques <a25968@alunos.ipca.pt>
2023-04-11 17:22:20 +0100

    Mapa

--- Content/sprites/tilesets/Grass.tsx
+++ Content/sprites/tilesets/Grass.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="Grass" tilewidth="64" tileheight="64" tilecount="8" columns="8">
- <image source="grassTiles.png" width="512" height="64"/>
+<tileset version="1.10" tiledversion="1.10.1" name="Grass" tilewidth="64" tileheight="64" tilecount="8" columns="8">
+ <image source="../tilesImages/grassTiles.png" width="512" height="64"/>
 </tileset>
--- Content/sprites/tilesets/buchoduplo.tsx
+++ Content/sprites/tilesets/buchoduplo.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="buchoduplo" tilewidth="128" tileheight="192" spacing="64" tilecount="1" columns="1">
- <image source="../FFimagens/buchoduplo.png" width="128" height="192"/>
+<tileset version="1.10" tiledversion="1.10.1" name="buchoduplo" tilewidth="128" tileheight="192" spacing="64" tilecount="1" columns="1">
+ <image source="../tilesImages/buchoduplo.png" width="128" height="192"/>
 </tileset>
