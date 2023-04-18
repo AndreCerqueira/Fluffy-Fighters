474e38f7ccb0e00f2c340e1e5e6c53dcda84bf4d
Andre Cerqueira <andrecerq16@gmail.com>
2023-04-18 11:19:08 +0100

    bug fixes pt1

--- Content/sprites/tilesets/Water2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="Water2" tilewidth="64" tileheight="64" tilecount="29" columns="29">
- <image source="Water.png" width="1856" height="64"/>
-</tileset>
