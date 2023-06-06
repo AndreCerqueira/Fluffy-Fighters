bc537789d35816e6d1bb84a147f12641c1eb9d2b
Andre Cerqueira <andrecerq16@gmail.com>
2023-06-06 15:39:50 +0100

    inicio sons

---
+++ Others/Sounds.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace FluffyFighters.Others
+{
+    public static class Sounds
+    {
+        public static SoundEffect main;
+        public static SoundEffect cut;
+        public static SoundEffect battle;
+        public static SoundEffect prepareBattle;
+        public static SoundEffect levelUp;
+
+        public static void LoadSounds(ContentManager Content)
+        {
+            main = Content.Load<SoundEffect>("AndTheJourneyBegins_");
+            cut = Content.Load<SoundEffect>("Cut");
+            battle = Content.Load<SoundEffect>("DecisiveBattle");
+            prepareBattle = Content.Load<SoundEffect>("PrepareForBattle");
+            levelUp = Content.Load<SoundEffect>("levelup");
+        }
+    }
+}
